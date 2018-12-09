3b34dd8893cc79fe8f37a44e96197f13904ea811
Fredrik Hov <fredrik.hov@knowit.no>
2018-12-09 16:44:22 +0100

    Update the arrays of inputs with the names of the tasks

--- src/com/crappyengineering/JulekalenderLoader.java
+++ src/com/crappyengineering/JulekalenderLoader.java
@@ -17,28 +17,28 @@ public class JulekalenderLoader {
     private static final Logger logger = LoggerFactory.getLogger(JulekalenderLoader.class);
 
     private final static Class[][] constructorParameters = {
-            {String.class},
-            {String.class},
-            {Long.class, Integer.class, Long.class},
-            {String.class, Integer.class},
-            {List.class},
-            {Long.class, Long.class},
-            {Void.class},
-            {Void.class},
-            {String.class, String.class, String.class}
+            {String.class}, // Luke 1 - Vekksort
+            {String.class}, // Luke 2 - Parallellregning
+            {Long.class, Integer.class, Long.class}, // Luke 3 - Juletall
+            {String.class, Integer.class}, // Luke 4 - Pokémonjakt
+            {List.class}, // Luke 5 - 42
+            {Long.class, Long.class}, // Luke 6 - Nulltunge tall
+            {Void.class}, // Luke 7 - Barmhjertig vekksort
+            {Void.class}, // Luke 8 - Matrjosjka Menagerie
+            {String.class, String.class, String.class} // Luke 9 - Hash-kjede
 
     };
 
     private final static Object[][] constructorInputs = {
-            {"https://s3-eu-west-1.amazonaws.com/knowit-julekalender-2018/input-vekksort.txt"},
-            {"https://s3-eu-west-1.amazonaws.com/knowit-julekalender-2018/input-rain.txt"},
-            {4294967296L, 24, 1000L},
-            {"https://s3-eu-west-1.amazonaws.com/knowit-julekalender-2018/input-pokemon-jakt.png", 8},
-            {asList(1, 2, 3, 4, 5, 6, 7, 8, 7, 6, 5, 4, 3, 2, 1)},
-            {1L, 18163106L},
-            {null},
-            {null},
-            {"https://s3-eu-west-1.amazonaws.com/knowit-julekalender-2018/input-hashchain.json", "MD5", "julekalender"}
+            {"https://s3-eu-west-1.amazonaws.com/knowit-julekalender-2018/input-vekksort.txt"}, // Luke 1 - Vekksort
+            {"https://s3-eu-west-1.amazonaws.com/knowit-julekalender-2018/input-rain.txt"}, // Luke 2 - Parallellregning
+            {4294967296L, 24, 1000L}, // Luke 3 - Juletall
+            {"https://s3-eu-west-1.amazonaws.com/knowit-julekalender-2018/input-pokemon-jakt.png", 8}, // Luke 4 - Pokémonjakt
+            {asList(1, 2, 3, 4, 5, 6, 7, 8, 7, 6, 5, 4, 3, 2, 1)}, // Luke 5 - 42
+            {1L, 18163106L}, // Luke 6 - Nulltunge tall
+            {null}, // Luke 7 - Barmhjertig vekksort
+            {"https://s3-eu-west-1.amazonaws.com/knowit-julekalender-2018/input-dolls.txt"}, // Luke 8 - Matrjosjka Menagerie
+            {"https://s3-eu-west-1.amazonaws.com/knowit-julekalender-2018/input-hashchain.json", "MD5", "julekalender"} // Luke 9 - Hash-kjede
     };
 
 
